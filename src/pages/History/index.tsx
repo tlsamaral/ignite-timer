ec1510f9d7a2cc88f1b36e742ddd1cc5c665bb68
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-11-21 20:50:56 -0300

    all

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,64 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
+export function History() {
+	return (
+		<HistoryContainer>
+			<h1>Meu historico</h1>
+
+			<HistoryList>
+				<table>
+					<thead>
+						<tr>
+							<th>Tarefa</th>
+							<th>Duração</th>
+							<th>Início</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Tarefa</td>
+							<td>20 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Tarefa</td>
+							<td>20 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Tarefa</td>
+							<td>20 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Tarefa</td>
+							<td>20 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Tarefa</td>
+							<td>20 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</HistoryList>
+		</HistoryContainer>
+	);
+}
